80bd19fca379ee0923628cedcc2da974c6bdd477
asyedabduljabbar-ui <asyedabduljabbar@gmail.com>
2026-03-03 09:24:16 +0530

    Syed Abdul Jabbar added uc3

---
+++ OOPSBannerApp/OOPSBannerUC3.java
@@ -0,0 +1,56 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS Banner App - UC3 using String.join()\n");
+
+        
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** "));
+
+        
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **     **",
+                " **      "));
+
+        
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **     **",
+                " **      "));
+
+        
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                "  ****** ",
+                "  *****  "));
+
+        
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **       ",
+                "       ** "));
+
+        
+        System.out.println(String.join(" ",
+                " **     **",
+                " **     **",
+                " **       ",
+                "      **  "));
+
+        
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " **       ",
+                " ******  "));
+    }
+}
